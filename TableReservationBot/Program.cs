ae50e1bbd5c388c8afb9cf62cad8459b9d63c8f9
aykutmayali <aykutmayali@gmail.com>
2020-03-18 12:30:02 +0300

    solutions

---
+++ TableReservationBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Syn.Bot.Oscova;
+
+namespace TableReservationBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new OscovaBot();
+            bot.ImportWorkspace(@"C:\Users\aykut\OneDrive\Documents\bot1.west");
+            bot.Trainer.StartTraining();
+
+            bot.MainUser.ResponseReceived += (sender, eventArgs) =>
+            {
+                Console.WriteLine($"Bot: {eventArgs.Response.Text}");
+            };
+            while (true)
+            {
+                var request = Console.ReadLine();
+                var evaluationResult = bot.Evaluate(request);
+                evaluationResult.Invoke();
+            }
+            
+        }
+    }
+}
